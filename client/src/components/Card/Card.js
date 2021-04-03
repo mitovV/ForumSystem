d491df4bc3eeeb91109cfa324b237f4805d2485c
CodingMania <vasilmitovv@gmail.com>
2021-04-03 12:18:11 +0300

    Small changes

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -4,30 +4,31 @@ import PostDetails from '../PostDetails'
 
 const Card = ({ post, showAddCommentForm }) => {
     return (
-        <>
-            <div className="col-md-12"></div>
-            <div className="card mb-4"></div>
-            <div className="card-header">
-                <div className="media flex-wrap w-100 align-items-center">
-                    <img src={post.creator.pictureUrl} width="50" className="d-block ui-w-40 rounded-circle" alt="user" />
-                    <div className="ml-3">
-                        {post.creator.username}
-                    </div>
-                    <div className="media-body text-right">
-                        <FcCalendar /><Moment local format="YYYY/MM/DD H:MM">{post.createdOn}</Moment>
+        <div className="container-fluid mt-100">
+            <div className="row">
+                <div className="col-md-12"></div>
+                <div className="card mb-4"></div>
+                <div className="card-header">
+                    <div className="media flex-wrap w-100 align-items-center">
+                        <img src={post.creator.pictureUrl} width="50" className="d-block ui-w-40 rounded-circle" alt="user" />
+                        <div className="ml-3">
+                            {post.creator.username}
+                        </div>
+                        <div className="media-body text-right">
+                            <FcCalendar /><Moment local format="YYYY/MM/DD H:MM">{post.createdOn}</Moment>
+                        </div>
                     </div>
                 </div>
+                <div className="card-body">
+                    <article>
+                        {post.content}
+                    </article>
+                    <div className="px-4 pt-3"> <button type="button" className="btn btn-primary float-right" onClick={showAddCommentForm}>Comment</button> </div>
+                    <div className="clearfix"></div>
+                </div>
+                {post.comments.length > 0 ? <PostDetails post={post.comments} showAddCommentForm={showAddCommentForm}/> : null}
             </div>
-            <div className="card-body">
-                <article>
-                    {post.content}
-                </article>
-                <div className="px-4 pt-3"> <button type="button" className="btn btn-primary float-right" onClick={showAddCommentForm}>Comment</button> </div>
-                <div className="clearfix"></div>
-            </div>
-            {post.comments.length > 0 ? <PostDetails post={post.comments} showAddCommentForm={showAddCommentForm} /> : null}
-        </>
-
+        </div>
     )
 }
 
